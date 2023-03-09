ca35263dfb57d13b123e6c533d8e6f578a3fd80f
nizsz <poha.de.cla@gmail.com>
2023-03-09 00:09:06 -0300

    initial commit

---
+++ src/routes/user.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+
+const userRoutes = Router();
+
+
+const usersController = new UsersController();
+
+userRoutes.post("/",usersController.create);
+userRoutes.put("/:id", usersController.update);
+
+module.exports = userRoutes;
