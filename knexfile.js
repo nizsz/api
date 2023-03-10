a25766edf074c4e8297467fb1bc7abe775e8c28e
nizsz <poha.de.cla@gmail.com>
2023-03-10 20:52:05 -0300

    created migrations with knex

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,10 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
+
+    migrations : {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
     useNullAsDefault : true
   }
 };
