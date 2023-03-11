d552040e912407511f3de2d39e4ced6edaadaafe
nizsz <poha.de.cla@gmail.com>
2023-03-10 21:48:15 -0300

    created migration for links

--- src/database/knex/migrations/20230311003011_createLinks.js
+++ src/database/knex/migrations/20230311003011_createLinks.js
@@ -1,15 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+exports.up = knex => knex.schema.createTable("links", table => {
+  table.increments("id");
+  table.text("url").notNullable();
+
+  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
+  table.timestamp("created_at").default(knex.fn.now());
   
-};
+
+});
+
+
+exports.down = knex => knex.schema.dropTable("links");
